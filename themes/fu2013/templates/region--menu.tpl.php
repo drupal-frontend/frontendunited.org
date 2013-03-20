67985bc15f8ba1f1e351e28f107b69b80b885861
Philippe Gervaise <philippe@malvese.com>
2013-03-20 23:14:52 +0100

    Fixes mobile menu

--- themes/fu2013/templates/region--menu.tpl.php
+++ themes/fu2013/templates/region--menu.tpl.php
@@ -8,19 +8,20 @@
 <div<?php print $attributes; ?>>
   <?php if ($main_menu || $secondary_menu): ?>
   <nav class="navigation">
+    <?php print l(t('Menu'), '', array('attributes' => array('class' => array('mobile-menu')))); ?>
     <?php print theme('links__system_main_menu', array(
-      'links' => $main_menu, 
+      'links' => $main_menu,
       'attributes' => array(
         'class' => array(
-          'links', 
-          'inline', 
-          'main-menu'
+          'links',
+          'inline',
+          'main-menu element-invisible'
           )
-        ), 
+        ),
       'heading' => array(
         'text' => t('Main menu'),
         'level' => 'h2',
-        'class' => array('element-invisible'          )
+        'class' => array('element-invisible')
         )
       )); ?>
   </nav>
